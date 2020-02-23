2de693ddf8d8f0a1c39d23b3b7da29e72eecbaaf
Erik A. Brandstadmoen <erik@brandstadmoen.net>
2020-02-23 12:30:55 +0100

    Added simple project demonstrating the problem

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace ConsoleApp
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Do(FlipFlop.Flip);
+        }
+
+        private static void Do(FlipFlop flipOrFlop)
+        {
+            Console.WriteLine("Does it flip or flop? " + flipOrFlop);
+        }
+    }
+}
